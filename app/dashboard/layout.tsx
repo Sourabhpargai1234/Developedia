329f4592bb6f952e3504940447331ef20ab08f5d
sourabh.pargai <sourabhpargai1234@gmail.com>
2024-09-02 21:08:05 +0530

    twelth commit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </div>
         <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
         <head>
-          <AdSense pId="ca-pub-2736406526636598"/>
+          <AdSense pId="2736406526636598"/>
         </head>
       </div>
     );
