8af9c3dc112138fd3c3c0b0a96b13ec01cf5b9b1
sourabh.pargai <sourabhpargai1234@gmail.com>
2024-09-03 11:11:41 +0530

    uiimprovement 1

--- app/dashboard/feeds/page.tsx
+++ app/dashboard/feeds/page.tsx
@@ -16,41 +16,41 @@ const FeedsPage = async () => {
   console.log(feeds)
 
   return (
-    <div className="max-w-5xl p-4 h-full flex-col">
-      <h1 className="text-2xl font-semibold text-center mb-6">Feeds</h1>
-      <ul className="grid w-full grid-cols-1 sm:grid-cols-2 gap-6">
-        {feeds.map((feed) => (
-          <li key={feed._id.toString()} className="bg-white rounded-lg shadow-md flex flex-col justify-between">
-            <div className="aspect-w-4 aspect-h-3/5">
-              <img
-                src={feed.file}
-                alt={feed.content}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="p-4">
-              <h2 className="text-lg font-medium">{feed.content}</h2>
-              <h2 className="text-gray-600">{feed.desc}</h2>
-              <Link href={`/dashboard/feeds/${feed.user.toString()}`}>
-                  <p className="text-gray-600 cursor-pointer">Author: {feed?.user?.username}</p>
-              </Link>
-            </div>
-            <div className="flex h-8 gap-8 mx-4 justify-between">
-                <LikeButton id={feed._id.toString()} />
-                <ChatBubbleLeftIcon className="cursor-pointer hover:opacity-80"/>
-                <FollowButton id={feed._id.toString()} />
-            </div>
-            <div className='flex my-4 mx-4 w-full'>
-              <LikedByButton id={feed._id.toString()}/>
-            </div>
-            <div className='float-right w-full text-center'>{feed.createdAt.toLocaleString()}</div>
-          </li>
-        ))}
-      </ul>
-      <div className='mt-8'>
-        <AdUnit />
-      </div>
+  <div className="max-w-5xl p-4 h-full flex-col">
+    <h1 className="text-2xl font-semibold text-center mb-6">Feeds</h1>
+    <ul className="grid w-full grid-cols-1 sm:grid-cols-2 gap-6">
+      {feeds.map((feed) => (
+        <li key={feed._id.toString()} className="bg-white rounded-lg shadow-md flex flex-col justify-between w-full sm:w-auto">
+          <div className="aspect-w-4 aspect-h-3/5">
+            <img
+              src={feed.file}
+              alt={feed.content}
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="p-4">
+            <h2 className="text-lg font-medium">{feed.content}</h2>
+            <h2 className="text-gray-600">{feed.desc}</h2>
+            <Link href={`/dashboard/feeds/${feed.user.toString()}`}>
+              <p className="text-gray-600 cursor-pointer">Author: {feed?.user?.username}</p>
+            </Link>
+          </div>
+          <div className="flex h-8 gap-8 mx-4 justify-between">
+            <LikeButton id={feed._id.toString()} />
+            <ChatBubbleLeftIcon className="cursor-pointer hover:opacity-80" />
+            <FollowButton id={feed._id.toString()} />
+          </div>
+          <div className="flex my-4 mx-4 w-full">
+            <LikedByButton id={feed._id.toString()} />
+          </div>
+          <div className="float-right w-full text-center">{feed.createdAt.toLocaleString()}</div>
+        </li>
+      ))}
+    </ul>
+    <div className="mt-8">
+      <AdUnit />
     </div>
+  </div>
   );
 };
 
