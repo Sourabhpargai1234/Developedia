b98a7903f771015b4ad602e72996154cab72a694
sourabh.pargai <sourabhpargai1234@gmail.com>
2024-09-06 19:53:20 +0530

    19 commit

--- app/ui/register/page.tsx
+++ app/ui/register/page.tsx
@@ -59,7 +59,7 @@ const Signup = () => {
       console.log(generateOtp.data.message);
 
       // Redirect to the OTP verification page
-      router.push("/dashboard/verifyotp");
+      router.push("/verifyotp");
 
     } catch (error) {
       console.log(error);
